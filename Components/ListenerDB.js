a1aec486097bd3afcaa1f7f0ed5e7a572a318cfc
vineeth <f20180233@hyderabad.bits-pilani.ac.in>
2021-05-09 11:46:33 +0530

    Minor missing functionality added

--- Components/ListenerDB.js
+++ Components/ListenerDB.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -7,14 +7,72 @@ import {
   StyleSheet,
   TouchableOpacity,
   ImageBackground,
-  Dimensions
+  Dimensions,
 } from "react-native";
 import { Icon } from "react-native-elements";
 
-const screenWidth= Dimensions.get('window').width;
-const screenHeight=Dimensions.get('window').height;
+import firebase from "firebase";
+require("firebase/firestore");
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const Register3 = ({ navigation }) => {
+  const [age, setAge] = useState();
+  const [topics, setTopics] = useState([]);
+  const [listenerName, setListenerName] = useState();
+
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    if (firebase.auth().currentUser) {
+      var currentUser = firebase.auth().currentUser.uid;
+      const db = firebase.firestore();
+
+      db.collection("Listeners")
+        .doc(currentUser)
+        .get()
+        .then((documentSnapshot) => {
+          if (documentSnapshot.exists) {
+            var data = documentSnapshot.data();
+
+            setAge(data["age"]);
+            setTopics(data["topics"]);
+            setListenerName(data["name"]);
+
+            console.log(age);
+
+            db.collection("ChatRequests").onSnapshot((querySnapshot) => {
+              const requestsList = [];
+
+              querySnapshot.forEach((documentSnapshot) => {
+                var minAge = documentSnapshot.data()["minAge"];
+                var maxAge = documentSnapshot.data()["maxAge"];
+                var topic = documentSnapshot.data()["topic"];
+
+                console.log(minAge);
+                console.log(maxAge);
+                console.log("Done");
+
+                if (minAge < Number(age) < maxAge) {
+                  console.log("entered");
+                  if (topics.includes(topic)) {
+                    requestsList.push({
+                      ...documentSnapshot.data(),
+                      key: documentSnapshot.id,
+                    });
+                  }
+                }
+              });
+
+              setNumber(requestsList.length);
+              console.log(number);
+            });
+          }
+        });
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styler.screen}>
       <ImageBackground
@@ -33,13 +91,13 @@ const Register3 = ({ navigation }) => {
                 name="person-circle"
                 type="ionicon"
                 color="#7AC141"
-                size={0.034*screenHeight}
+                size={24}
               />
             </TouchableOpacity>
           </View>
           <View style={styler.imageView}>
             <Image
-              style={{ width: 0.1*screenHeight, height: 0.1*screenHeight }}
+              style={{ width: 0.1 * screenHeight, height: 0.1 * screenHeight }}
               source={require("../assets/logoTB.png")}
             />
           </View>
@@ -58,14 +116,18 @@ const Register3 = ({ navigation }) => {
           >
             <View style={styler.find}>
               <Text
-                style={{ color: "white", textAlign: "center", fontSize: 0.03*screenHeight }}
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  fontSize: 0.03 * screenHeight,
+                }}
               >
                 FIND MY{"\n"}SEEKER
               </Text>
             </View>
           </TouchableOpacity>
           <View style={styler.activeUser}>
-            <Text style={{ color: "#ffffff" }}>0</Text>
+            <Text style={{ color: "#ffffff" }}>{number}</Text>
           </View>
         </View>
         <View style={styler.talkingNow}>
@@ -76,7 +138,7 @@ const Register3 = ({ navigation }) => {
             style={{
               flexDirection: "column",
               alignItems: "center",
-              marginBottom: 0.025*screenHeight,
+              marginBottom: 0.025 * screenHeight,
             }}
           >
             <TouchableOpacity
@@ -89,7 +151,7 @@ const Register3 = ({ navigation }) => {
                 name="chat"
                 type="material-icons"
                 color="#7AC141"
-                size={0.03*screenHeight}
+                size={24}
               />
             </TouchableOpacity>
             <Text>Dedicated</Text>
@@ -99,7 +161,7 @@ const Register3 = ({ navigation }) => {
             style={{
               flexDirection: "column",
               alignItems: "center",
-              marginBottom: 0.025*screenHeight,
+              marginBottom: 0.025 * screenHeight,
             }}
           >
             <TouchableOpacity
@@ -112,7 +174,7 @@ const Register3 = ({ navigation }) => {
                 name="journal"
                 type="ionicon"
                 color="#7AC141"
-                size={0.03*screenHeight}
+                size={24}
               />
             </TouchableOpacity>
             <Text>Journal</Text>
@@ -122,7 +184,7 @@ const Register3 = ({ navigation }) => {
             style={{
               flexDirection: "column",
               alignItems: "center",
-              marginBottom: 0.025*screenHeight,
+              marginBottom: 0.025 * screenHeight,
             }}
           >
             <TouchableOpacity>
@@ -131,7 +193,7 @@ const Register3 = ({ navigation }) => {
                 name="gift"
                 type="font-awesome-5"
                 color="#7AC141"
-                size={0.03*screenHeight}
+                size={24}
                 reverseColor="white"
               />
             </TouchableOpacity>
@@ -155,33 +217,33 @@ const styler = StyleSheet.create({
     justifyContent: "center",
   },
   head: {
-    width: 0.35*screenWidth,
+    width: 0.35 * screenWidth,
     alignItems: "flex-start",
     justifyContent: "flex-start",
-    marginTop: 0.045*screenHeight,
+    marginTop: 0.045 * screenHeight,
   },
   text: {
-    fontSize: 0.033*screenHeight,
+    fontSize: 0.033 * screenHeight,
     fontWeight: "bold",
     textAlign: "center",
   },
   textSmall: {
-    fontSize: 0.026*screenHeight,
-    margin: 0.015*screenHeight,
+    fontSize: 0.026 * screenHeight,
+    margin: 0.015 * screenHeight,
     textAlign: "center",
   },
   find: {
     backgroundColor: "#7AC141",
-    width: 0.42*screenWidth,
-    height: 0.42*screenWidth,
-    borderRadius: 0.13*screenHeight,
+    width: 0.42 * screenWidth,
+    height: 0.42 * screenWidth,
+    borderRadius: 0.13 * screenHeight,
     alignItems: "center",
     justifyContent: "center",
   },
   headerView: {
     flex: 0.3,
     flexDirection: "row",
-    padding: 0.015*screenHeight,
+    padding: 0.015 * screenHeight,
   },
   textView: {
     flex: 0.1,
@@ -190,35 +252,35 @@ const styler = StyleSheet.create({
     flex: 0.4,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 0.025*screenHeight,
+    marginBottom: 0.025 * screenHeight,
     flexDirection: "row",
   },
   footerView: {
     flexDirection: "row",
     flex: 0.2,
-    marginTop: 0.1*screenHeight,
-    marginLeft: 0.015*screenHeight,
-    marginRight: 0.015*screenHeight,
+    marginTop: 0.1 * screenHeight,
+    marginLeft: 0.04 * screenHeight,
+    marginRight: 0.04 * screenHeight,
     justifyContent: "space-around",
     alignItems: "center",
   },
   imageView: {
-    marginTop: 0.055*screenHeight,
+    marginTop: 0.055 * screenHeight,
   },
   talkingNow: {
     justifyContent: "center",
     alignItems: "center",
   },
   tnText: {
-    fontSize: 0.036*screenHeight,
+    fontSize: 0.036 * screenHeight,
   },
   activeUser: {
-    width: 0.1*screenWidth,
-    height: 0.1*screenWidth,
-    borderRadius: 0.05*screenWidth,
+    width: 0.1 * screenWidth,
+    height: 0.1 * screenWidth,
+    borderRadius: 0.05 * screenWidth,
     backgroundColor: "red",
     justifyContent: "center",
     alignItems: "center",
-    marginLeft: -0.025*screenHeight,
+    marginLeft: -0.025 * screenHeight,
   },
 });
